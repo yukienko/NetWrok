030f2ccc72018c8dfa043c86ac77ffcc7f12f975
Unknown <yukineko1442@gmail.com>
2019-12-21 16:06:05 +0900

    ネットワーク系の勉強をはじめた

---
+++ 12_18_2v2/Program.cs
@@ -0,0 +1,70 @@
+﻿//抽象クラスを使うメリット
+//プロジェクト名が間違ってます→12_18_1v2(こっちが正しい)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_18_1v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird[] b = new Bird[2]; //Birdクラスの変数の配列を生成
+            b[0] = new Crow();
+            b[1] = new Sparrow();
+            for(int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i].Name+" : ");
+                b[i].Sing();
+            }
+        }
+    }
+
+    class Crow : Bird
+    {
+        //コンストラクタ
+        public Crow() : base("カラス") { }
+        //カラスがなく
+        public override void Sing()
+        {
+            Console.WriteLine("カーカー");
+        }
+    }
+    class Sparrow : Bird
+    {
+        //コンストラクタ
+        public Sparrow() : base("すずめ") { }
+        //すずめがなく
+        public override void Sing()
+        {
+            Console.WriteLine("チュンチュン");
+        }
+    }
+    abstract class Bird
+    {
+        //名前フィールド
+        private String name;
+        //引数付きコンストラクタ
+        public Bird(String name)
+        {
+            this.name = name;
+        }
+        //名前を取得
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        //鳴く（抽象メソッド）
+        public abstract void Sing();
+    }
+}
+
+/*
+ * abstract : 抽象メソッド
+ */
---
+++ 12_18_2v2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("12_18_2v2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12_18_2v2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("3d542eb8-23b0-4334-8793-6b13a098e164")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
